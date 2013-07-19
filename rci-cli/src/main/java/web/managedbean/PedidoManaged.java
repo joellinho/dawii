693ff3b834dfd071721e5901e358ee7f1abe74e8
Alvaro <aethgh@gmail.com>
2013-07-19 17:31:09 -0500

    edw

---
+++ rci-cli/src/main/java/web/managedbean/PedidoManaged.java
@@ -0,0 +1,10 @@
+package web.managedbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="pedidoManaged")
+@SessionScoped
+public class PedidoManaged {
+
+}
